363032af5a28686e383dbe966da9444061d7014e
Anton <a.budanov@ngs.ru>
2022-05-06 00:56:48 +0300

    Final version of CaesarCode.java is added

--- CaesarCode.java
+++ CaesarCode.java
@@ -85,13 +85,13 @@ public class CaesarCode {
         fileFrom = scanner.nextLine();
 
         try {
-            int lastSlash = fileFrom.lastIndexOf("\\");                 //Определяем позицию последнего \ в имени файла
-            fileTo = fileFrom.substring(0, lastSlash);                   //Получаем путь к папке, где хранится исходный файл
+            int lastSlash = fileFrom.lastIndexOf("\\");          //Определяем позицию последнего \ в имени файла
+            fileTo = fileFrom.substring(0, lastSlash);               //Получаем путь к папке, где хранится исходный файл
             if (variant == 1) {
-                fileTo = fileTo + "\\caesarcode.txt";                       //Файл для зашифрованного сообщения
+                fileTo = fileTo + "\\caesarcode.txt";                //Файл для зашифрованного сообщения
             }
             else {
-                fileTo = fileTo + "\\decryption.txt";                       //Файл для расшифрованного сообщения
+                fileTo = fileTo + "\\decryption.txt";                //Файл для расшифрованного сообщения
             }
         }
         catch (RuntimeException e) {
@@ -100,7 +100,7 @@ public class CaesarCode {
         }
 
         System.out.println("Ищу файл и выполняю задание:");
-        for (int i = 0; i < 10; i++) {                                      //Имитируем кропотливую работу
+        for (int i = 0; i < 10; i++) {                               //Имитируем кропотливую работу
             System.out.print("\u231B ");
             Thread.sleep(600);
         }
@@ -111,15 +111,15 @@ public class CaesarCode {
         try(FileReader reader = new FileReader(fileFrom);
             FileWriter writer = new FileWriter(fileTo)) {
             while (reader.ready()) {
-                char x = (char)reader.read();                           //Читаем байт из потока и переводим его в символ
+                char x = (char)reader.read();                          //Читаем символ из потока
 
-                int position = alphabet.indexOf(x);                     //Определяем текущую позицию символа в алфавите
-                int sec_position = position + key;                      //Секретная позиция = позиция в алфавите + ключ
+                int position = alphabet.indexOf(x);                    //Определяем текущую позицию символа в алфавите
+                int sec_position = position + key;                     //Секретная позиция = позиция в алфавите + ключ
 
-                if (position + key >= alphabet.length()) {              //Если позиция + ключ выходит за пределы длины алфавита
-                    sec_position = sec_position - alphabet.length();    //Секретная позиция переходит в начало алфавита
+                if (sec_position >= alphabet.length()) {               //Если позиция + ключ выходит за пределы длины алфавита
+                    sec_position = sec_position - alphabet.length();   //Секретная позиция переходит в начало алфавита
                 }
-                writer.write(alphabet.charAt(sec_position));            //Пишем зашифрованный символ в файл
+                writer.write(alphabet.charAt(sec_position));           //Пишем зашифрованный символ в файл
             }
         }
         catch (IOException e) {
@@ -142,36 +142,37 @@ public class CaesarCode {
             badFile = true;
         }
 
-            //Цикл для перебора всех вариантов ключей
-            for (int i = 0; i < alphabet.length(); i++) {
-                key = i;
+        //Цикл для перебора всех вариантов ключей
+        for (int i = 0; i < alphabet.length(); i++) {
+            key = i;
 
-                //Цикл для декодировки текста
-                //Цикл начинается с 1, т.к. в позиции 0 стоит служебный символ, который файл txt добавляет перед текстом
-                //Если на других компьютерах txt не добавляет служебных символов, то int j следует указать = 0
-                for (int j = 1; j < text.length(); j++) {
-                    char x = text.charAt(j);                                    //Пишем в переменную текущий символ
-                    int position = alphabet.indexOf(x);                         //Определяем текущую позицию символа в алфавите
-                    int new_position = position - key;                          //Новая позиция: позиция в алфавите - ключ
+            //Цикл для декодирования текста
+            //Цикл начинается с 1, т.к. в позиции 0 стоит служебный символ, который файл txt добавляет перед текстом
+            //Если на иных компьютерах txt не добавляет служебных символов или добавляет больше, то j следует скоррект.
+            for (int j = 1; j < text.length(); j++) {
+                char x = text.charAt(j);                                 //Пишем в переменную текущий символ
+                int position = alphabet.indexOf(x);                      //Определяем текущую позицию символа в алфавите
+                int new_position = position - key;                       //Новая позиция: позиция в алфавите - ключ
 
-                    if (new_position < 0) {                                     //Если новая позиция становится отрицательной
-                        new_position = alphabet.length() + new_position;        //Новая позиция переходит в конец алфавита
-                    }
-                    result = result + alphabet.charAt(new_position);            //Собираем результат декодировки
+                if (new_position < 0) {                                  //Если новая позиция становится отрицательной
+                    new_position = alphabet.length() + new_position;     //Новая позиция переходит в конец алфавита
                 }
+                result = result + alphabet.charAt(new_position);         //Собираем результат декодировки
+            }
 
-                cryptoAnalyse(result);                                          //Осуществляем анализ получившейся строки
+            cryptoAnalyse(result);                                       //Осуществляем анализ получившейся строки
 
-            result = "";                                                        //Обнуляем результат для новой сборки
+        result = "";                                                     //Обнуляем результат для новой сборки
         }
 
+        //Определяем максимальный балл, набранный по логике ниже, и записываем в файл вариант с наибольшим баллом
         int max = 0;
         for(Map.Entry<Integer, String> pair : versions.entrySet()) {                 //Перебираем значения в Мар
             max = pair.getKey() > max ? pair.getKey() : max;                         //Ищем максимальное значение
         }
 
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileTo))) {
-            writer.write(versions.get(max));                                           //Записываем результат в файл
+            writer.write(versions.get(max));                                         //Записываем результат в файл
         }
         catch (IOException | NullPointerException e) {
             System.out.println("Возможно, указан неверный адрес.");
@@ -191,33 +192,33 @@ public class CaesarCode {
         * 9. Текст должен набрать хотя бы 2 балла.
         * Количество баллов выбрано по значимости параметра в логике обычного построения письменной речи.
         */
-        result = result.trim();                                             //Удаляем лишние пробелы, если они есть
+        result = result.trim();                                         //Удаляем лишние пробелы, если они есть
 
-        if (!result.contains(" ")) {                                        //1
+        if (!result.contains(" ")) {                                    //1
             return;
         }
-        if (result.matches("\\p{Punct}.*")) {                       //2
+        if (result.matches("\\p{Punct}.*")) {                     //2
             return;
         }
 
-        if (result.matches(".*(\\?|:|,|!|\\.)[а-яА-Я]+.*")) {         //3 Написание этого выражения заняло два вечера
+        if (result.matches(".*(\\?|:|,|!|\\.)[а-яА-Я]+.*")) {     //3 Написание этого выражения заняло два вечера
             return;
         }
 
         //Варианты, прошедшие отсеивание по пп. 1 - 3, набирают баллы
         int bonus = 0;
-        String[] sentense = result.split("\\s");                      //Делим текст, чтобы выделить первое слово
-        if (sentense[0].matches("[А-Я][а-я]*(,|:)*")) {               //4
+        String[] sentense = result.split("\\s");                  //Делим текст, чтобы выделить первое слово
+        if (sentense[0].matches("[А-Я][а-я]*(,|:)*")) {           //4
             bonus = bonus + 3;
         }
-        else if (sentense[0].matches("[А-Я]*|[а-я]*(,|:)*")) {        //5
+        else if (sentense[0].matches("[А-Я]*|[а-я]*(,|:)*")) {    //5
             bonus = bonus + 2;
         }
 
-        if (result.matches(".*(, |: | - ).*")) {                      //6
+        if (result.matches(".*(, |: | - ).*")) {                  //6
             bonus = bonus + 1;
         }
-        if (result.matches(".*(\\.|\\?|!)")) {                        //7
+        if (result.matches(".*(\\.|\\?|!)")) {                    //7
             bonus = bonus + 1;
         }
 
@@ -236,7 +237,7 @@ public class CaesarCode {
             return;
         }
 
-        versions.put(bonus, result);                                      //Записываем значение в Map
+        versions.put(bonus, result);                                 //Записываем значение в Map
     }
 
     public static void report() {
